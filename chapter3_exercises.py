e6a93a325f0b8a3a53b64e30675b08230dd29359
Mike <mlattanz25@gmail.com>
2013-07-18 21:12:12 -0400

    add progress

--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,33 @@
 # Exercises for chapter 3:
 
-# Name:
+# 3.1 Answer: name 'repeat_lyrics' is not defined
+
+#3.2 - The equation is only running once. 
+
+def print_lyrics():	
+	print "I'm a lumberjack, and I'm okay."
+	print "I sleep all night and I work all day."
+
+def repeat_lyrics():
+	print_lyrics()
+	print_lyrics()
+	print_lyrics()
+
+repeat_lyrics()
+
+#3.3: Must put equation before s
+
+def right_justify(s):
+	print (70 - len(s) ) * (" ") + s
+	
+right_justify ("allen")
+
+
+
+
+
+
+
+
+	
+
